bb7a2280c376f1935d9e704588f58836fbd9861a
Johnny G <john.groton@mymail.champlain.edu>
2022-03-20 18:12:30 -0400

    adding fully working movement

--- BannanaGame/Assets/Scripts/playerMovement.cs
+++ BannanaGame/Assets/Scripts/playerMovement.cs
@@ -4,20 +4,84 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
-    private GridManeger grid;
-    private float padding = 0.1f;
+    [SerializeField] private GridManeger grid;
     private Vector2Int cords;
+    private Vector2Int moveDir;
+
+    Tile currentTile;
 
     private void Start()
     {
         cords = new Vector2Int(0, 0);
-        Tile currentTile = grid.GetTile(cords);
+        currentTile = grid.GetTile(cords);
         transform.position = currentTile.transform.position;
     }
 
     private void Update()
     {
-        
+        getInput();
+    }
+
+    private void move(Vector2Int moveDirection)
+    {
+        cords = cords + moveDirection;
+        currentTile = grid.GetTile(cords);
+        transform.position = currentTile.transform.position;
+    }
+
+    private void getInput()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            moveDir = new Vector2Int(1, 0);
+            if (checkTile(moveDir + cords))
+            {
+                move(moveDir);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            moveDir = new Vector2Int(-1, 0);
+            if (checkTile(moveDir + cords))
+            {
+                move(moveDir);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            moveDir = new Vector2Int(0, 1);
+            if (checkTile(moveDir + cords))
+            {
+                move(moveDir);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            moveDir = new Vector2Int(0, -1);
+            if (checkTile(moveDir + cords))
+            {
+                move(moveDir);
+            }
+        }
+    }
+
+    private bool checkTile(Vector2Int targetCord)
+    {
+        if (targetCord.y >= grid.numRows || targetCord.x >= grid.numCollomns)
+        {
+            return false;
+        }
+        else if (targetCord.y < 0 || targetCord.x < 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
     }
 
 
