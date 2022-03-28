af8e788a7b29b3f5b21aced9d5f5446ffe00877b
Johnny G <john.groton@mymail.champlain.edu>
2022-03-28 11:10:10 -0400

    adding ui sounds to main menu

--- BannanaGame/Assets/Scripts/mainMenu.cs
+++ BannanaGame/Assets/Scripts/mainMenu.cs
@@ -5,13 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class mainMenu : MonoBehaviour
 {
+    AudioSource uiClick;
+
+    private void Start()
+    {
+        uiClick = GetComponent<AudioSource>();
+    }
+
     public void Play()
     {
+        uiClick.PlayOneShot(uiClick.GetComponent<AudioClip>());
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void Quit()
     {
+        uiClick.PlayOneShot(uiClick.GetComponent<AudioClip>());
         Application.Quit();
     }
 }
